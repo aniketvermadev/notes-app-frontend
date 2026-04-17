09ba9cf2f768367a60ed85d969e1eb7ada554161
Aniket <aniketverma.seraphic@gmail.com>
2026-04-17 10:17:58 +0530

    Url changes

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://notes-app-backend-435q.onrender.com/api",
   timeout: 10000
 });
 
